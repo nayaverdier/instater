2f0886dc8ed6e1f40e2ae5b4ac7a01c3130baca1
Naya Verdier <nayaverdier@users.noreply.github.com>
2021-10-24 20:02:22 -0700

    Fix tests for deployment

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,10 +1,10 @@
 def test_module_import():
     import instater
 
-    assert instater.__all__ == ["InstaterError", "VERSION", "run_tasks", "tasks"]
+    assert instater.__all__ == ["InstaterError", "VERSION", "run_tasks"]
 
 
 def test_direct_import():
     from instater import VERSION  # noqa: F401
     from instater import InstaterError  # noqa: F401
-    from instater import tasks  # noqa: F401
+    from instater import run_tasks  # noqa: F401
